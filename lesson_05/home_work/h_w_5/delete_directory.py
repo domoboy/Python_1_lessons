d47e3f37f831a66736894f3d33990bd08f8ae998
domoboy <nkonoplyannikov@gmail.com>
2018-11-01 16:13:15 +0300

    добавил функцию удаления папок

--- lesson_05/home_work/h_w_5/delete_directory.py
+++ lesson_05/home_work/h_w_5/delete_directory.py
@@ -1,1 +1,20 @@
+# импортируем модуль os
+import os
+import sys
+
+
+def del_directory(name):
+
+    os.chdir(os.path.dirname(sys.argv[0]))
+
+    # обращаюсь к папкам
+    dir_path = os.path.join(os.getcwd(), name)
+
+    # удаляю папки и обрабатываю ошибки
+    try:
+        for i in range(1, 10):
+            os.rmdir(dir_path + str(i))
+
+    except OSError:
+        print('Нельзя удалить то, чего не существует')
 
--- lesson_05/home_work/hw05_easy.py
+++ lesson_05/home_work/hw05_easy.py
@@ -3,6 +3,18 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import sys
+import os
+import delete_directory as dld
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+# delete_directory.del_directory('dir_')
+
+
+print('os.getcwd = ', os.getcwd())
+print('sys.argv = ', sys.argv)
+
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
